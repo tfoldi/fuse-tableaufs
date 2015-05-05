5cf4e2391a934abc819e7cd247feeed6baa46038
Tamas Foldi <tfoldi@starschema.net>
2015-05-05 13:40:45 +0000

    Merge master

--- src/tableaufs.c
+++ src/tableaufs.c
@@ -1,151 +1,107 @@
-/* E: Filesystem in Userspace
-  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
-  This program can be distributed under the terms of the GNU GPL.
-  See the file COPYING.
-*/
+/*
+   Copyright (c) 2015, Tamas Foldi, Starschema
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+   1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+   THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+   IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+   THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   */
+
 #define FUSE_USE_VERSION 30
-#include <fuse_lowlevel.h>
+#include <fuse.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <assert.h>
-static const char *tableau_str = "Hello World!\n";
-static const char *tableau_name = "tableau";
-static int tableau_stat(fuse_ino_t ino, struct stat *stbuf)
+#include "workgroup.h"
+
+static const char *tableau_str = "tableau World!\n";
+static const char *tableau_path = "/tableau";
+
+static int tableau_getattr(const char *path, struct stat *stbuf)
 {
-        stbuf->st_ino = ino;
-        switch (ino) {
-        case 1:
-                stbuf->st_mode = S_IFDIR | 0755;
-                stbuf->st_nlink = 2;
-                break;
-        case 2:
-                stbuf->st_mode = S_IFREG | 0444;
-                stbuf->st_nlink = 1;
-                stbuf->st_size = strlen(tableau_str);
-                break;
-        default:
-                return -1;
-        }
-        return 0;
+  int res = 0;
+  tfs_wg_node_t node;
+
+  TFS_WG_parse_path(path, &node);
+
+  
+  memset(stbuf, 0, sizeof(struct stat));
+  if (strcmp(path, "/") == 0) {
+    stbuf->st_mode = S_IFDIR | 0755;
+    stbuf->st_nlink = 2;
+  } else if (strcmp(path, tableau_path) == 0) {
+    stbuf->st_mode = S_IFREG | 0444;
+    stbuf->st_nlink = 1;
+    stbuf->st_size = strlen(tableau_str);
+  } else
+    res = -ENOENT;
+  return res;
 }
-static void tableau_getattr(fuse_req_t req, fuse_ino_t ino,
-                             struct fuse_file_info *fi)
+
+static int tableau_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+    off_t offset, struct fuse_file_info *fi)
 {
-        struct stat stbuf;
-        (void) fi;
-        memset(&stbuf, 0, sizeof(stbuf));
-        if (tableau_stat(ino, &stbuf) == -1)
-                fuse_reply_err(req, ENOENT);
-        else
-                fuse_reply_attr(req, &stbuf, 1.0);
+  (void) offset;
+  (void) fi;
+  if (strcmp(path, "/") != 0)
+    return -ENOENT;
+  filler(buf, ".", NULL, 0);
+  filler(buf, "..", NULL, 0);
+  filler(buf, tableau_path + 1, NULL, 0);
+  return 0;
 }
-static void tableau_lookup(fuse_req_t req, fuse_ino_t parent, const char *name)
+
+static int tableau_open(const char *path, struct fuse_file_info *fi)
 {
-        struct fuse_entry_param e;
-        if (parent != 1 || strcmp(name, tableau_name) != 0)
-                fuse_reply_err(req, ENOENT);
-        else {
-                memset(&e, 0, sizeof(e));
-                e.ino = 2;
-                e.attr_timeout = 1.0;
-                e.entry_timeout = 1.0;
-                tableau_stat(e.ino, &e.attr);
-                fuse_reply_entry(req, &e);
-        }
+  if (strcmp(path, tableau_path) != 0)
+    return -ENOENT;
+  if ((fi->flags & 3) != O_RDONLY)
+    return -EACCES;
+  return 0;
 }
-struct dirbuf {
-        char *p;
-        size_t size;
+
+static int tableau_read(const char *path, char *buf, size_t size, off_t offset,
+    struct fuse_file_info *fi)
+{
+  size_t len;
+  (void) fi;
+  if(strcmp(path, tableau_path) != 0)
+    return -ENOENT;
+  len = strlen(tableau_str);
+  if (offset < len) {
+    if (offset + size > len)
+      size = len - offset;
+    memcpy(buf, tableau_str + offset, size);
+  } else
+    size = 0;
+  return size;
+}
+
+static struct fuse_operations tableau_oper = {
+  .getattr        = tableau_getattr,
+  .readdir        = tableau_readdir,
+  .open           = tableau_open,
+  .read           = tableau_read,
 };
-static void dirbuf_add(fuse_req_t req, struct dirbuf *b, const char *name,
-                       fuse_ino_t ino)
-{
-        struct stat stbuf;
-        size_t oldsize = b->size;
-        b->size += fuse_add_direntry(req, NULL, 0, name, NULL, 0);
-        b->p = (char *) realloc(b->p, b->size);
-        memset(&stbuf, 0, sizeof(stbuf));
-        stbuf.st_ino = ino;
-        fuse_add_direntry(req, b->p + oldsize, b->size - oldsize, name, &stbuf,
-                          b->size);
-}
-#define min(x, y) ((x) < (y) ? (x) : (y))
-static int reply_buf_limited(fuse_req_t req, const char *buf, size_t bufsize,
-                             off_t off, size_t maxsize)
-{
-        if (off < bufsize)
-                return fuse_reply_buf(req, buf + off,
-                                      min(bufsize - off, maxsize));
-        else
-                return fuse_reply_buf(req, NULL, 0);
-}
-static void tableau_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
-                             off_t off, struct fuse_file_info *fi)
-{
-        (void) fi;
-        if (ino != 1)
-                fuse_reply_err(req, ENOTDIR);
-        else {
-                struct dirbuf b;
-                memset(&b, 0, sizeof(b));
-                dirbuf_add(req, &b, ".", 1);
-                dirbuf_add(req, &b, "..", 1);
-                dirbuf_add(req, &b, tableau_name, 2);
-                reply_buf_limited(req, b.p, b.size, off, size);
-                free(b.p);
-        }
-}
-static void tableau_open(fuse_req_t req, fuse_ino_t ino,
-                          struct fuse_file_info *fi)
-{
-        if (ino != 2)
-                fuse_reply_err(req, EISDIR);
-        else if ((fi->flags & 3) != O_RDONLY)
-                fuse_reply_err(req, EACCES);
-        else
-                fuse_reply_open(req, fi);
-}
-static void tableau_read(fuse_req_t req, fuse_ino_t ino, size_t size,
-                          off_t off, struct fuse_file_info *fi)
-{
-        (void) fi;
-        assert(ino == 2);
-        reply_buf_limited(req, tableau_str, strlen(tableau_str), off, size);
-}
-static struct fuse_lowlevel_ops tableau_oper = {
-        .lookup         = tableau_lookup,
-        .getattr        = tableau_getattr,
-        .readdir        = tableau_readdir,
-        .open           = tableau_open,
-        .read           = tableau_read,
-};
+
 int main(int argc, char *argv[])
 {
-        struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-        struct fuse_chan *ch;
-        char *mountpoint;
-        int err = -1;
-        if (fuse_parse_cmdline(&args, &mountpoint, NULL, NULL) != -1 &&
-            (ch = fuse_mount(mountpoint, &args)) != NULL) {
-                struct fuse_session *se;
-                se = fuse_lowlevel_new(&args, &tableau_oper,
-                                       sizeof(tableau_oper), NULL);
-                if (se != NULL) {
-                        if (fuse_set_signal_handlers(se) != -1) {
-                                fuse_session_add_chan(se, ch);
-                                /* Block until ctrl+c or fusermount -u */
-                                err = fuse_session_loop(se);
-                                fuse_remove_signal_handlers(se);
-                                fuse_session_remove_chan(ch);
-                        }
-                        fuse_session_destroy(se);
-                }
-                fuse_unmount(mountpoint, ch);
-        }
-        fuse_opt_free_args(&args);
-        return err ? 1 : 0;
+  TFS_WG_connect_db( "localhost", "5432", "postgres", "readonly" );
+
+  return fuse_main(argc, argv, &tableau_oper, NULL);
 }
